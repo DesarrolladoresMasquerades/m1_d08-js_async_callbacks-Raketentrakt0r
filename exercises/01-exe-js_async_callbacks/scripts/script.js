ecb5588b02985d14be981b7dd4ee439cb6aee41c
Luis Moncada <raketentrakt0r@gmail.com>
2022-01-26 17:07:35 +0100

    done!

--- exercises/01-exe-js_async_callbacks/scripts/script.js
+++ exercises/01-exe-js_async_callbacks/scripts/script.js
@@ -7,12 +7,23 @@ function print() {
     console.log(4);
   }
   
-  print(); 
+  print(console.log);
+  
   
   // ------------------------------------------------------------------------
   
   
-  //Exercise 2: Write a function blast() , which loops over the numbers 1 to 15. every 1 second 
+  //Exercise 2: Write a function blast() , which loops over the numbers 1 to 15. every 1 second  
+  function blast(){
+   
+    for (let i = 0; i < 15; i++){
+      if (3 % 15 === 0){
+        return BOOM
+      } else if (5 % 15 === 0){
+        return BANG
+      }
+    }
+  }
   // - For every number divisible by 3 it prints BOOM!!
   // - For every number divisible by 5 it prints a BANG!!
   // - Forevery number divisible for 3 and 5 it prints BOOM BANG!! 
